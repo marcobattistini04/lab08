2b5adfcc39efbfbb40973687819ecc3e25e7517f
Marco Battistini <marco.battistini10@studio.unibo.it>
2024-11-12 09:26:55 +0100

    modified DrawNumberConsoleview in order to print a beginning message and print only attempt results. It does not communicate with the controller

---
+++ 83-mvc-gui-reflection/src/main/java/it/unibo/mvc/view/DrawNumberConsoleview.java
@@ -0,0 +1,24 @@
+package it.unibo.mvc.view;
+
+import it.unibo.mvc.api.DrawNumberController;
+import it.unibo.mvc.api.DrawNumberView;
+import it.unibo.mvc.api.DrawResult;
+
+public class DrawNumberConsoleview implements DrawNumberView{
+    
+    @Override
+    public void setController(DrawNumberController observer) {
+
+    }
+
+    @Override
+    public void start() {
+        System.out.println("Send inputs on the other views in order to write on the console");
+    }
+
+    @Override
+    public void result(DrawResult res) {
+        System.out.println(res.getDescription());
+    }
+    
+}
